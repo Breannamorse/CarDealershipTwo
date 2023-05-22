c5636bdaa711270227bf9af0a7dfd8f3b0ae2ee6
Bmorse-coder <bmorse@appdev.yearup.org>
2023-05-21 21:19:34 -0400

    Add new files

--- src/main/java/com/learntocode/ContractDataManager.java
+++ src/main/java/com/learntocode/ContractDataManager.java
@@ -1,4 +1,25 @@
 package com.learntocode;
 
+import java.io.FileWriter;
+
 public class ContractDataManager {
+    String filePath;
+
+    public ContractDataManager(String filePath);
+        this.filePath = filePath;
+
+    public void saveContract(Contract contract) {
+        try (FileWriter = new FileWriter("Contract.csv")){
+    if (contract instanceof SalesContract) {
+        SalesContract salesContract = (SalesContract) contract;
+        writer.write("Sales Contract:\n");
+        writer.write("Buyer: " + salesContract.getBuyer() + "\n");
+        writer.write("Seller: " + salesContract.getSeller() + "\n");
+        writer.write("Amount: " + salesContract.getAmount() + "\n");
+    }
+
+        }
+    }
 }
+
+
