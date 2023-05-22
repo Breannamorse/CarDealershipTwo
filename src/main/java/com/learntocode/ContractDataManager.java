d38122b341ed9ff65a9ccb99a2a52c207fe2ad60
Bmorse-coder <bmorse@appdev.yearup.org>
2023-05-22 00:15:35 -0400

    Add leasing contract

--- src/main/java/com/learntocode/ContractDataManager.java
+++ src/main/java/com/learntocode/ContractDataManager.java
@@ -1,25 +1,32 @@
 package com.learntocode;
 
 import java.io.FileWriter;
+import java.io.*;
 
 public class ContractDataManager {
-    String filePath;
 
-    public ContractDataManager(String filePath);
-        this.filePath = filePath;
+    public void saveContract (Contract contract) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("contracts.csv",true))) {
+            if (contract instanceof SalesContract){
+                bw.write("SALES" + "|" + contract.getDate() + "|" + contract.getCustomerName() + "|" + contract.getCustomerEmail() + "|" +
+                        contract.getVehicleSold().getVin() + "|" + contract.getVehicleSold().getYear() + "|" + contract.getVehicleSold().getMake()
+                        + "|" + contract.getVehicleSold().getModel() + "|" + contract.getVehicleSold().getVehicleType() + "|" + contract.getVehicleSold()
+                        .getColor() + "|" + contract.getVehicleSold().getOdometer() + "|" + contract.getVehicleSold().getPrice() + "|" + ((SalesContract) contract).getSalesTaxAmount() +
+                        "|" + ((SalesContract) contract).getRecordingFee() + "|" + ((SalesContract) contract).getProcessingFee() + "|" + contract.getTotalPrice() + "|" + ((SalesContract) contract).isFinancing()
+                        + "|" + contract.getMonthlyPayment());
+            } else if (contract instanceof LeasingContract) {
+                bw.write("LEASE" + "|" + contract.getDate() + "|" + contract.getCustomerName() + "|" + contract.getCustomerEmail() + "|" +
+                        contract.getVehicleSold().getVin() + "|" + contract.getVehicleSold().getYear() + "|" + contract.getVehicleSold().getMake()
+                        + "|" + contract.getVehicleSold().getModel() + "|" + contract.getVehicleSold().getVehicleType() + "|" + contract.getVehicleSold()
+                        .getColor() + "|" + contract.getVehicleSold().getOdometer() + "|" + contract.getVehicleSold().getPrice() + "|" + ((LeasingContract) contract).getExpectedEndingValue() + "|"
+                        + ((LeasingContract) contract).getLeaseFee() + "|" + contract.getTotalPrice() + "|" + contract.getMonthlyPayment());
+            }
+            bw.newLine();
 
-    public void saveContract(Contract contract) {
-        try (FileWriter = new FileWriter("Contract.csv")){
-    if (contract instanceof SalesContract) {
-        SalesContract salesContract = (SalesContract) contract;
-        writer.write("Sales Contract:\n");
-        writer.write("Buyer: " + salesContract.getBuyer() + "\n");
-        writer.write("Seller: " + salesContract.getSeller() + "\n");
-        writer.write("Amount: " + salesContract.getAmount() + "\n");
-    }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
 
         }
-    }
-}
-
-
+    }}
